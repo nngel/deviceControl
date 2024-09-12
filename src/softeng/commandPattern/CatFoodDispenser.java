ef5ec1a0763528e0c65cb7a1a6abf518d198342e
nngel <edwardangellorica@gmail.com>
2024-09-12 21:53:42 +0800

    9-12-24

--- src/softeng/commandPattern/CatFoodDispenser.java
+++ src/softeng/commandPattern/CatFoodDispenser.java
@@ -1,4 +1,18 @@
 package softeng.commandPattern;
 
-public class CatFoodDispenser {
+public class CatFoodDispenser extends Device{
+    private int foodLvl;
+
+    public CatFoodDispenser() {
+        this.foodLvl = 100;
+    }
+    public void dispense() {
+        foodLvl--;
+    }
+    public int getFoodLvl() {
+        return foodLvl;
+    }
+    public void refill(int foodLvl) {
+        this.foodLvl = foodLvl;
+    }
 }
